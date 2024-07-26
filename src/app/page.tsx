f7b3b5dadf4ab4b8051236c607fb40a651d3ad4a
ludavidca <davidjylu7@gmail.com>
2024-07-26 14:46:41 -0400

    prisma db init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,26 +15,7 @@ export default function Home() {
 
   return (
     <div>
-      <div className="h-20 bg-transparent"></div>
-      <div className="flex flex-col justify-center overflow-y-auto">
-        <form onSubmit={getMessages}>
-          <button type="submit">QueryDB</button>
-        </form>
-        {/* {messages.map((message) => (
-          <div className={`flex-row max-h-screen`}>
-              <p
-                className={`rounded-lg border border-black p-4 mx-10 my-5 ${
-                  parseInt(message.sent_to) % 2 === 0
-                    ? "bg-white"
-                    : "bg-rose-300"
-                }`}
-              >
-                {message.content}
-              </p>
-          </div>
-        ))} */}
-      </div>
-      <div className="h-20 bg-transparent"></div>
+      <p>Work in Progress</p>
     </div>
   );
 }
