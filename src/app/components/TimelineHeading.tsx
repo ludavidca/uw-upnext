3df4efc133882923e708b89f63752abb90aa9a4d
ludavidca <davidjylu7@gmail.com>
2024-08-10 17:33:11 -0400

    same for timeline

--- src/app/components/TimelineHeading.tsx
+++ src/app/components/TimelineHeading.tsx
@@ -1,8 +1,13 @@
 // src/app/components/SectionHeading.js
-export default function TimelineHeading({ text }) {
-    return (
-      <div className="px-5 sm:px-10 py-3">
-        <h1 className="text-sm font-semibold text-gray-100">{text}</h1>
-      </div>
-    );
-  } 
+
+interface TimeLineHeadingProps {
+  text: string;
+}
+
+export default function TimelineHeading({ text }: TimeLineHeadingProps) {
+  return (
+    <div className="px-5 sm:px-10 py-3">
+      <h1 className="text-sm font-semibold text-gray-100">{text}</h1>
+    </div>
+  );
+} 
