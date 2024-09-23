670809fd7dea55deda31c75e330a4744a33106d3
Madhav Malik <madhavmalik25@gmail.com>
2024-09-22 22:42:11 -0400

    added images

--- WebScraper/OldScripts/test.py
+++ WebScraper/OldScripts/test.py
@@ -1,31 +1,19 @@
 import requests
 import os
-from bs4 import BeautifulSoup
-from urllib.parse import urlparse
-import pandas as pd
-
-postsDf = pd.read_csv("WebScraper\Data\instagram_raw.csv").replace('"','', regex=True)
-
-
-import requests
-import os
 import pandas as pd
 from urllib.parse import urlparse
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 def download_instagram_image(url, folder_path, postID):
     try:
-        # Send a GET request to the image URL directly
         response = requests.get(url, stream=True)
-        response.raise_for_status()  # Raise an exception for bad status codes
+        response.raise_for_status()
         
-        # Create the folder if it doesn't exist
         os.makedirs(folder_path, exist_ok=True)
         
-        # Generate a filename from the URL
         filename = postID + ".jpg"
         file_path = os.path.join(folder_path, filename)
         
-        # Save the image
         with open(file_path, 'wb') as file:
             for chunk in response.iter_content(chunk_size=8192):
                 file.write(chunk)
@@ -34,17 +22,21 @@ def download_instagram_image(url, folder_path, postID):
     except requests.exceptions.RequestException as e:
         print(f"Failed to download the image: {e}")
 
+def process_row(row):
+    imageurl = str(row.display_photo)
+    postID = str(row.url)
+    filepath = os.path.join('public', 'InstagramImages')
+    print(f"Downloading: {imageurl}")
+    download_instagram_image(imageurl, filepath, postID)
+
 # Use raw string for file path
-postsDf = pd.read_csv(r"WebScraper\Data\instagram_raw.csv").replace('"','', regex=True)
-cnt = 0
-for index, post in postsDf.iterrows():
-    if cnt <= 5:
-        imageurl = str(post.display_photo)
-        postID = str(post.url)
-        filepath = os.path.join('public', 'InstagramImages')
-        print(f"Downloading: {imageurl}")
-        download_instagram_image(imageurl, filepath, postID)
-        # Remove the break statement to process all rows
-        cnt +=1
-    else:
-        break
+postsDf = pd.read_csv(r"/Users/madhavmalik/VSC Projects/uw-upnext/WebScraper/Data/instagram_raw.csv").replace('"','', regex=True)
+
+# Use ThreadPoolExecutor to download images in parallel
+with ThreadPoolExecutor(max_workers=10) as executor:
+    futures = [executor.submit(process_row, row) for _, row in postsDf.iterrows()]
+    
+    for future in as_completed(futures):
+        future.result()  # This will raise any exceptions that occurred during execution
+
+print("All downloads completed.")
